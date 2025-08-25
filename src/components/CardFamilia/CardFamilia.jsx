7cca8fa7095cfaed4defd703ff77e51190073c79
PEDRO HENRIQUE WARDENSKI DE PAULA <pet0159953@aluno.firjansenaisesi.com.br>
2025-08-25 19:09:48 -0300

    card props pronto e card da família pronto

--- src/components/CardFamilia/CardFamilia.jsx
+++ src/components/CardFamilia/CardFamilia.jsx
@@ -1,60 +1,17 @@
-import { useState } from "react";
 import styles from "./CardFamilia.module.css";
+import { CardsProps } from "../CardsProps/CardsProps";
 
 export const CardFamilia = () => {
-  const mensagens = [
-    { id: 1, nome: "Rosi", texto: "lorem", urlImg: "" },
-    { id: 2, nome: "Julio", texto: "lorem", urlImg: "" },
-    { id: 3, nome: "Jamir", texto: "lorem", urlImg: "" },
-    { id: 4, nome: "Lucas", texto: "lorem", urlImg: "" },
-    { id: 5, nome: "Mateus", texto: "lorem", urlImg: "" },
-    { id: 6, nome: "Jailane", texto: "lorem", urlImg: "" },
-    { id: 7, nome: "Dona Aparecida", texto: "lorem", urlImg: "" },
-    { id: 8, nome: "Dona fátima", texto: "lorem", urlImg: "" },
-  ];
-
-  const [cardAmpliado, setCardAmpliado] = useState(null);
-
-  const toggleCard = (id) => {
-    setCardAmpliado(cardAmpliado === id ? null : id);
-  };
-
   return (
     <>
-      <h1>Card da família</h1>
-      <div className={styles.container}>
-        <div className={styles.grid}>
-          {mensagens.map((mensagem) => (
-            <div
-              key={mensagem.id}
-              className={cardAmpliado === mensagem.id ? styles.ampliado : ""}
-              onClick={() => toggleCard(mensagem.id)}
-            >
-              <div className={styles.imgCOntainer}>
-                <img src={mensagem.urlImg} alt={mensagem.nome} />
-              </div>
-              <div className={styles.textContainer}>
-                <p className={styles.text}>
-                  {cardAmpliado === mensagem.id
-                    ? mensagem.texto
-                    : mensagem.texto.length > 25
-                    ? mensagem.texto.slice(0, 25) + "..."
-                    : mensagem.texto}
-                </p>
-                {mensagem.texto.length > 25 && (
-                  <button
-                    className={styles.button}
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      toggleCard(mensagem.id);
-                    }}
-                  ></button>
-                )}
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
+      <CardsProps nome="Rosi" mensagem="" />
+      <CardsProps nome="Dona Aparecida" mensagem="" />
+      <CardsProps nome="Júlio" mensagem="" />
+      <CardsProps nome="Lucas" mensagem="" />
+      <CardsProps nome="Mateus" mensagem="" />
+      <CardsProps nome="Jamir" mensagem="" />
+      <CardsProps nome="Jailane" mensagem="" />
+      <CardsProps nome="Dona Fátima" mensagem="" />
     </>
   );
 };
--- src/components/CardsProps/CardsProps.jsx
+++ src/components/CardsProps/CardsProps.jsx
@@ -1,1 +1,9 @@
-export const CardsProps = () => {};
+export const CardsProps = ({ imagem, nome, mensagem }) => {
+  return (
+    <div>
+      <img src={imagem} alt={nome} />
+      <h3>{nome}</h3>
+      <p>{mensagem}</p>
+    </div>
+  );
+};
