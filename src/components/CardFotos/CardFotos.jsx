c998a34d21899d1610b6aa6033c79de49acaa048
CesarCouto <cesarrcouto@outlook.com>
2025-08-27 02:09:18 -0300

    principal pronto

--- src/components/CardFotos/CardFotos.jsx
+++ src/components/CardFotos/CardFotos.jsx
@@ -1,18 +1,33 @@
 import Styles from "./CardFotos.module.css"
 import { CardFotosProps } from "../CardFotosProps/CardFotosProps";
-
-
+import Imagem1 from "../../images/Imagem1.webp"
+import Imagem2 from "../../images/Imagem2.webp"
+import Imagem3 from "../../images/Imagem3.webp"
 
 export const CardFotos = () => {
-
-
-
-
-
-  return (
+return (
   <div className={Styles.fotos}>
-  <h1>Essas serão as fotos</h1>;
-  <CardFotosProps />
+
+    <div className={Styles.Container_texto}>
+        <h1 className={Styles.Titulo}>🎉 Parabéns, João! 🎂</h1>;
+        <p className={Styles.Texto}>Que o seu dia seja cheio de alegria, sorrisos e boas lembranças. 
+          Que cada novo ano traga mais conquistas, saúde e felicidade. Você merece tudo de melhor hoje e sempre. 
+          Aproveite muito o seu aniversário! 🥳</p>
+    </div>
+
+    <div className={Styles.Container_imagens}>
+        <CardFotosProps 
+          imagem={Imagem1}
+        />
+        <CardFotosProps 
+          imagem={Imagem2}
+        />
+        <CardFotosProps 
+          imagem={Imagem3}
+        />
+    </div>
+  
+ 
   </div>
   );
 
--- src/components/CardFotosProps/CardFotosProps.jsx
+++ src/components/CardFotosProps/CardFotosProps.jsx
@@ -2,9 +2,10 @@ import Styles from "./CardFotosProps.module.css";
 import Perfil from "../../images/Perfil.jpg"
 import Imagem1 from "../../images/Imagem1.webp"
 import Favorito from "../../images/Favorito.png"
+import Coracao from "../../images/Coracao.png"
+import Comentario from "../../images/Comentario.png"
 
-
-export const CardFotosProps = () => {
+export const CardFotosProps = ({imagem}) => {
 return (
     <>
         <div className={Styles.Card}>
@@ -21,14 +22,15 @@ return (
             </div>
 
             <div className={Styles.Imagem}>
-                <img className={Styles.Imagens} src={Imagem1} alt="" />
+                <img className={Styles.Imagens} src={imagem} alt="" />
 
             </div>
 
             <div className={Styles.Status}>
 
                 <div className={Styles.Status1}>
-
+                    <img className={Styles.Icone2} src={Coracao} alt="" />
+                    <img className={Styles.Icone3} src={Comentario} alt="" />
                 </div>
 
                 <div className={Styles.Status2}>
