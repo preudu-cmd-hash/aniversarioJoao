91c955cece5f8094d44b182e7204f3b1c77d7303
CesarCouto <cesarrcouto@outlook.com>
2025-08-27 03:13:37 -0300

    Pronto

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,3 +1,36 @@
+import Styles from "./Footer.module.css"
+import Feliz from "../../images/Feliz.png"
+import Balao_Esquerda from "../../images/Balao_Esquerda.png"
+import Balao_Direita from "../../images/Balao_Direita.png"
+
+
+
 export const Footer = () => {
-  return <h1>Footer</h1>;
+
+
+  return (
+    <div className={Styles.Footer}>
+
+      <div className={Styles.Aniversario}>
+
+        <div className={Styles.ContainerE}>
+            <img className={Styles.ImagemF} src={Balao_Esquerda} alt="" />
+        </div>
+
+        <div className={Styles.ContainerF}>
+            <img className={Styles.ImagemF} src={Feliz} alt="" />
+        </div>
+
+        <div className={Styles.ContainerD}>
+            <img className={Styles.ImagemF} src={Balao_Direita} alt="" />
+        </div>
+
+      </div>
+
+      <div className={Styles.Criadores}>
+          <p className={Styles.Texto_Criador}>Criado por César, Pedro, Gallo e Moon</p>
+      </div>
+    
+    </div>
+  ); 
 };
