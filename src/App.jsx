57ac26605fc7d6d86a2ec89e64a6f37ca4885f4e
Pedro <pedrohenrique010123456@gmail.com>
2025-08-27 01:10:16 -0300

    remoção de coisas inutilziadas

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
-import { useState } from "react";
 import { CardFotos } from "./components/CardFotos/CardFotos";
 import { Footer } from "./components/Footer/Footer";
-import { Carousel } from "react-bootstrap";
 import { CardFamilia } from "./components/CardFamilia/CardFamilia";
 import { CardAmigos } from "./components/CardAmigos/CardAmigos";
 
 function App() {
-  const [activeIndex, setActiveIndex] = useState(0);
-
-  const handleSelect = (selectedIndex) => {
-    setActiveIndex(selectedIndex);
-  };
-
   return (
     <>
       <div>
